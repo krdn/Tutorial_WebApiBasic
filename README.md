1ae5dd987d505c6310b5b34c687bfb17958e2ffe
Gon <krdn.net@gmail.com>
2023-10-04 22:23:48 +0900

    #8 Migration 실행
    close #8

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 [ASPNET6 WebApi Basic Tutorial 1/2](https://youtu.be/2Jxu0K25Csc?si=YZUkTiSphz5ruzAI)
 
 [ASPNET6 WebApi Basic Tutorial 2/2](https://youtu.be/OFG9cYsILWI?si=q9W4g7XcVS2WDg_9)
+
+
+
+```bash
+
+dotnet ef migrations add InitialCreate --context MyDbContext --output-dir Migrations/MsSqlMigrations --project Tutorial_WebApiBasic --startup-project Tutorial_WebApiBasic
+
+dotnet ef migrations remove --project Tutorial_WebApiBasic
+
+dotnet ef database update --context MyDbContext --project Tutorial_WebApiBasic --startup-project Tutorial_WebApiBasic
+
+
+// 변경 후 새롭게 갱신(속성 No 추가)
+dotnet ef migrations add AddNo --context MyDbContext --output-dir Migrations/MsSqlMigrations --project Tutorial_WebApiBasic --startup-project Tutorial_WebApiBasic
+
+
+// 변경 후 새롭게 갱신(속성 No 삭제)
+dotnet ef migrations add RemoveNo --context MyDbContext --output-dir Migrations/MsSqlMigrations --project Tutorial_WebApiBasic --startup-project Tutorial_WebApiBasic
+
+// 마지막 추가된 migration삭제하기
+dotnet ef migrations remove --context MyDbContext --project Tutorial_WebApiBasic --startup-project Tutorial_WebApiBasic
+
+```
+
+
---
+++ Tutorial_WebApiBasic/Migrations/MsSqlMigrations/20231004132046_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tutorial_WebApiBasic.Migrations.MsSqlMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
