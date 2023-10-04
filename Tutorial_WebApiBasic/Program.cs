a371ac9db53bb8da5ffac960d6ecb06275331f62
Gon <krdn.net@gmail.com>
2023-10-04 09:50:03 +0900

    #5
    close #5

--- Tutorial_WebApiBasic/Program.cs
+++ Tutorial_WebApiBasic/Program.cs
@@ -1,8 +1,23 @@
+using Serilog;
+
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateBootstrapLogger();
+
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
+
+// https://learn.microsoft.com/ko-kr/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-7.0
+builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
