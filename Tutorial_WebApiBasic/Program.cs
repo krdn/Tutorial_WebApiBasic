f2a54b4dbaf2e4afd043b3c17b742e32ca7e9317
Gon <krdn.net@gmail.com>
2023-10-04 22:35:25 +0900

    try catch 추가

--- Tutorial_WebApiBasic/Program.cs
+++ Tutorial_WebApiBasic/Program.cs
@@ -14,20 +14,40 @@ using Microsoft.AspNetCore.Builder;
 //    .ReadFrom.Configuration(configuration)
 //    .CreateBootstrapLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// https://learn.microsoft.com/ko-kr/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-7.0
-builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-builder.Services.AddDbContext<MyDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+//try
+//{
 
-var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.MapControllers();
-app.Run();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    // https://learn.microsoft.com/ko-kr/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-7.0
+    builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+    builder.Services.AddDbContext<MyDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+
+    var app = builder.Build();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapControllers();
+
+    // https://learn.microsoft.com/ko-kr/aspnet/core/mvc/controllers/routing?view=aspnetcore-7.0
+    //app.MapControllerRoute(
+    //    name: "default",
+    //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+//}
+//catch (Exception ex)
+//{
+//    string type = ex.GetType().Name;
+//    if (type.Equals("StopTheHostException", StringComparison.OrdinalIgnoreCase)) throw;
+//    Log.Fatal(ex, "Host terminated unexpectedly");
+//}
+//finally
+//{
+//    Log.CloseAndFlush();
+//}
